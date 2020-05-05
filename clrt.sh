0aa3e025b23e7b49644b9331550f0ad112e4888b
rivu <58026024+unphydra@users.noreply.github.com>
2020-05-05 18:26:56 +0530

    modified clrt to run the test file in tests directory

--- clrt.sh
+++ clrt.sh
@@ -1,21 +1,19 @@
 #! /bin/bash
 
-runFile="test.c"
+runFile='tests/test.c'
 executable=$1
 
 rm -rf *.o
 
-if [[ -n $executable ]]
-  then
-    runFile=$executable
-fi
 list=($(ls))
 for file in ${list[*]};
 do 
-  if [ $file != *$runFile* ] && [[ $file == *".c"* ]]
-    then gcc -c $file
+  if [ $file != *$executable* ] && [[ $file == *".c"* ]]
+    then 
+        gcc -c $file
   fi
 done
+gcc -c $runFile
 gcc -o main *.o
 ./main
 rm -rf *.o main
